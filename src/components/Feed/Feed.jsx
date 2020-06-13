4c537b7a79f52906c5db7926be80d8ccc964ae64
Romain LEMBO <romainlembo06@gmail.com>
2020-06-13 15:38:39 +0200

    [init-feed] Grid components ok

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Card, CardImage, CardContent } from "components/Card";
+import { Col, Row } from 'components/Grid';
 
 export default function Feed() {
     return (
         <Card>
-            <CardImage />
-            <CardContent>Feed</CardContent>
+            <Row>
+                <Col size={1}>
+                    <CardImage />
+                </Col>
+                <Col size={4}>
+                    <CardContent>Feed</CardContent>
+                </Col>
+            </Row>
         </Card>
     )
 }
---
+++ src/components/Grid/index.js
@@ -0,0 +1,3 @@
+export { default as Grid } from 'components/Grid/Grid';
+export { default as Row } from 'components/Grid/Row';
+export { default as Col } from 'components/Grid/Col';
