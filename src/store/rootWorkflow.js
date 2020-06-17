879e81d08557d15d03dd12950115ee22e186e9f4
Romain LEMBO <romainlembo06@gmail.com>
2020-06-17 16:39:38 +0200

    [unit-tests] unit-tests for saga / workflow OK

--- src/store/rootWorkflow.js
+++ src/store/rootWorkflow.js
@@ -3,11 +3,9 @@ import { spawn, all } from 'redux-saga/effects';
 import { GenericWorkflow } from 'store/generic';
 
 export default function* (apis) {
-    const usersWorkflows = new GenericWorkflow('users').runWorkflow;
     const activitiesWorkflows = new GenericWorkflow('feeds').runWorkflow;
 
     yield all([
-        spawn(usersWorkflows(apis)),
         spawn(activitiesWorkflows(apis)),
     ])
 }
